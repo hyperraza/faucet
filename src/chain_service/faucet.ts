2926df77c871442248446070e54263e608603d35
gianfra-t <96739519+gianfra-t@users.noreply.github.com>
2024-04-17 09:07:03 -0300

    Update src/chain_service/faucet.ts
    Co-authored-by: Marcel Ebert <mail@marcel-ebert.de>

--- src/chain_service/faucet.ts
+++ src/chain_service/faucet.ts
@@ -200,7 +200,7 @@ export default class Faucet {
         this.registerLackOfFundsEvent();
     }else{
         console.log(
-          "\x1b[31m%s\x1b[0m",
+          "\x1b[31m",
           "🚫 Encountered some other error: ",
           error?.toString(),
           JSON.stringify(error),
