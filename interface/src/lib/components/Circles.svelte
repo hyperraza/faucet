2b1f119834e1753af8e8ce301fc17d5651d5545f
Raza <hyperraza@users.noreply.github.com>
2024-07-29 19:04:42 +0200

    Run formatting

--- interface/src/lib/components/Circles.svelte
+++ interface/src/lib/components/Circles.svelte
@@ -1,18 +1,14 @@
+<img src="circle.svg" alt="just circle" class="absolute right-0 hidden scale-150 md:block top-3" />
 <img
-src="circle.svg"
-alt="just circle"
-class="absolute right-0 hidden scale-150 md:block top-3"
-/>
-<img
-src="circle.svg"
-alt="just circle"
-class="absolute hidden scale-125 md:block -right-3 top-1/2"
+	src="circle.svg"
+	alt="just circle"
+	class="absolute hidden scale-125 md:block -right-3 top-1/2"
 />
 <img src="circle.svg" alt="just circle" class="absolute bottom-2 left-5" />
 <img
-src="circle.svg"
-alt="just circle"
-class="absolute hidden scale-125 md:block -left-3 top-1/2"
+	src="circle.svg"
+	alt="just circle"
+	class="absolute hidden scale-125 md:block -left-3 top-1/2"
 />
 <img src="circle.svg" alt="just circle" class="absolute hidden left-1/2 md:block top-1/2" />
 <img src="circle.svg" alt="just circle" class="absolute scale-150 right-1/2 top-2/3" />
--- interface/src/lib/components/FaucetForm.svelte
+++ interface/src/lib/components/FaucetForm.svelte
@@ -19,6 +19,7 @@
 			}
 		} catch (error) {
 			message = 'An error occurred. Please try again later.';
+			console.log(error);
 		} finally {
 			isPending = false;
 		}
@@ -26,31 +27,29 @@
 </script>
 
 <form autocomplete="off" class="mt-5 text-lg" on:submit={handleSubmit}>
-    <label for="foucoco_address" class="text-gray-200">Foucoco address</label>
-    <input
-        type="text"
-        placeholder="2fa9..."
-        required
-        class="w-full px-2 py-1 text-xl text-gray-200 border rounded bg-inherit md:min-w-96"
-        id="foucoco_address"
-        name="foucoco_address"
-        autocomplete="off"
-        data-lpignore="true"
-        data-1p-ignore="true"
-        bind:value={address}
-    />
-    <button
-        disabled={isPending}
-        aria-busy={isPending}
-        type="submit"
-        class="w-full px-8 py-2 mt-4 font-bold text-white transition rounded-full bg-emerald-500 hover:bg-emerald-700 disabled:hover:bg-emerald-500 disabled:opacity-25"
-        >{isPending ? 'Submitting...' : 'GET AMPE (testnet)'}</button
-    >
+	<label for="foucoco_address" class="text-gray-200">Foucoco address</label>
+	<input
+		type="text"
+		placeholder="2fa9..."
+		required
+		class="w-full px-2 py-1 text-xl text-gray-200 border rounded bg-inherit md:min-w-96"
+		id="foucoco_address"
+		name="foucoco_address"
+		autocomplete="off"
+		data-lpignore="true"
+		data-1p-ignore="true"
+		bind:value={address}
+	/>
+	<button
+		disabled={isPending}
+		aria-busy={isPending}
+		type="submit"
+		class="w-full px-8 py-2 mt-4 font-bold text-white transition rounded-full bg-emerald-500 hover:bg-emerald-700 disabled:hover:bg-emerald-500 disabled:opacity-25"
+		>{isPending ? 'Submitting...' : 'GET AMPE (testnet)'}</button
+	>
 </form>
 {#if message}
-    <p
-        class="w-full max-w-3xl mt-4 text-sm text-center text-red-500 break-words whitespace-pre-line"
-    >
-        {message}
-    </p>
+	<p class="w-full max-w-3xl mt-4 text-sm text-center text-red-500 break-words whitespace-pre-line">
+		{message}
+	</p>
 {/if}
